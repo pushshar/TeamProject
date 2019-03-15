dbcc534851106feae2ac70e277f8430950ea0b68
KRISTHAK <KRISTHAK@DIN51003398.corp.capgemini.com>
2019-03-15 16:03:37 +0530

    These are the changes by Krishan Narayan Thakur

--- EmployeeProject/src/com/capgemini/repo/EmployeeRepoImpl.java
+++ EmployeeProject/src/com/capgemini/repo/EmployeeRepoImpl.java
@@ -1,18 +1,33 @@
 package com.capgemini.repo;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import com.capgemini.beans.Employee;
 
 public class EmployeeRepoImpl implements EmployeeRepo {
+	HashMap<Integer,Employee> employeeList=new HashMap<>();
 
 	@Override
 	public Employee save(Employee employee) {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.find(employee.getEmployeeId())==null)
+		{
+			employeeList.put(employee.getEmployeeId(), employee);
+			return employee;
+		}
+		else
+		{
+			return null;
+		}
 	}
 
 	@Override
 	public Employee find(int employeeId) {
-		// TODO Auto-generated method stub
+		for(Entry<Integer,Employee> entry:employeeList.entrySet())
+		{
+			if(entry.getKey()==employeeId)
+				return (entry.getValue());
+		}
 		return null;
 	}
 
