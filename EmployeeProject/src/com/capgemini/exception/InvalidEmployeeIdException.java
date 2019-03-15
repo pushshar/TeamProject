132ccbddeff9b5b04ff54c11457ab596b17c9dd4
ARVAGRAH <ARVAGRAH@DIN51003425.corp.capgemini.com>
2019-03-15 14:39:42 +0530

    Last change by Arvind

--- EmployeeProject/src/com/capgemini/exception/InvalidEmployeeIdException.java
+++ EmployeeProject/src/com/capgemini/exception/InvalidEmployeeIdException.java
@@ -2,4 +2,18 @@ package com.capgemini.exception;
 
 public class InvalidEmployeeIdException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5002785498217853671L;
+
+	public InvalidEmployeeIdException(String message) {
+		super(message);
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage();
+	}
+
 }
--- EmployeeProject/src/com/capgemini/exception/NoDataFoundException.java
+++ EmployeeProject/src/com/capgemini/exception/NoDataFoundException.java
@@ -2,4 +2,18 @@ package com.capgemini.exception;
 
 public class NoDataFoundException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3771524605423364446L;
+
+	public NoDataFoundException(String message) {
+		super(message);
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage();
+	}
+
 }
